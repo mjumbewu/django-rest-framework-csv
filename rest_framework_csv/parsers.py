15836700afe1ed06b939843c41854533a80e13a8
Sebastien Beal <sebastibe@gmail.com>
2013-04-28 12:33:38 +0200

    Add flat file CSV parser with customizable delimiter

---
+++ rest_framework_csv/parsers.py
@@ -0,0 +1,31 @@
+import csv
+
+from rest_framework.parsers import BaseParser
+from rest_framework.exceptions import ParseError
+
+
+class CSVParser(BaseParser):
+    """
+    Parses CSV serialized data.
+
+    The parser assumes the first line contains the column names.
+    """
+
+    media_type = 'text/csv'
+
+    def parse(self, stream, media_type=None, parser_context=None):
+        parser_context = parser_context or {}
+        delimiter = parser_context.get('delimiter', ',')
+
+        try:
+            rows = csv.reader(stream, delimiter=delimiter)
+            header = rows.next()
+            data = []
+            for row in rows:
+                row_data = {}
+                for i, item in enumerate(row):
+                    row_data.update({header[i]: item})
+                data.append(row_data)
+            return data
+        except Exception, exc:
+            raise ParseError('CSV parse error - %s' % unicode(exc))
