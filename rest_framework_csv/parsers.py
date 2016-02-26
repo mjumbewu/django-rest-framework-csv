4b239015f5033d16c12ca5ddf5813e97556c5f5a
Brandon Roberts <brandon@bxroberts.org>
2016-02-25 17:27:50 -0800

    use unicodecsv and make parsing more robust

--- rest_framework_csv/parsers.py
+++ rest_framework_csv/parsers.py
@@ -1,4 +1,5 @@
-import csv
+import unicodecsv as csv
+#import csv
 import codecs
 import io
 import six
@@ -9,37 +10,20 @@ from rest_framework.exceptions import ParseError
 from rest_framework_csv.orderedrows import OrderedRows
 
 
-def preprocess_stream(stream, charset):
-    if six.PY2:
-        # csv.py doesn't do Unicode; encode temporarily:
-        return (chunk.encode(charset) for chunk in stream)
-    else:
-        return stream
-
-def postprocess_row(row, charset):
-    if six.PY2:
-        # decode back to Unicode, cell by cell:
-        return [cell.decode(charset) for cell in row]
-    else:
-        return row
-
 def unicode_csv_reader(csv_data, dialect=csv.excel, charset='utf-8', **kwargs):
-    csv_data = preprocess_stream(csv_data, charset)
     csv_reader = csv.reader(csv_data, dialect=dialect, **kwargs)
     for row in csv_reader:
-        yield postprocess_row(row, charset)
+        yield row
 
 def universal_newlines(stream):
-    for intermediate_line in stream:
-        # It's possible that the stream was not opened in universal
-        # newline mode. If not, we may have a single "row" that has a
-        # bunch of carriage return (\r) characters that should act as
-        # newlines. For that case, lets call splitlines on the row. If
-        # it doesn't have any newlines, it will return a list of just
-        # the row itself.
-        for line in intermediate_line.splitlines():
-            yield line
-
+    # It's possible that the stream was not opened in universal
+    # newline mode. If not, we may have a single "row" that has a
+    # bunch of carriage return (\r) characters that should act as
+    # newlines. For that case, lets call splitlines on the row. If
+    # it doesn't have any newlines, it will return a list of just
+    # the row itself.
+    for line in stream.splitlines():
+        yield line
 
 class CSVParser(BaseParser):
     """
@@ -53,14 +37,26 @@ class CSVParser(BaseParser):
     def parse(self, stream, media_type=None, parser_context=None):
         parser_context = parser_context or {}
         delimiter = parser_context.get('delimiter', ',')
+        encoding = parser_context.get('encoding', settings.DEFAULT_CHARSET)
 
-        try:
-            encoding = parser_context.get('encoding', settings.DEFAULT_CHARSET)
-            rows = unicode_csv_reader(universal_newlines(stream), delimiter=delimiter, charset=encoding)
-            data = OrderedRows(next(rows))
-            for row in rows:
-                row_data = dict(zip(data.header, row))
-                data.append(row_data)
-            return data
-        except Exception as exc:
-            raise ParseError('CSV parse error - %s' % str(exc))
+        # take whatever data we have and convert to byte array (str)
+        #print "\n*** stream", stream, type(stream)
+        strdata = str(stream.read().encode('utf-8'))
+        #print "strdata", strdata
+        #binary = io.BytesIO(strdata)
+        binary = universal_newlines( strdata)
+        #print "d ****", dir(binary)
+        rows = unicode_csv_reader(binary, delimiter=delimiter, charset=encoding)
+        #print "rows", rows
+        #rows = next(rows)
+        #print "rows next", rows
+        data = OrderedRows(next(rows))
+        #print "\n*** data", data
+        for row in rows:
+            #print "parsed row", row
+            #print "data.header", data.header
+            row_data = dict(zip(data.header, row))
+            data.append(row_data)
+        return data
+        #except Exception as exc:
+        #    raise ParseError('CSV parse error - %s' % str(exc))
