8eceb695cd023ad069ecb759230b549916fbc45f
Awais Qureshi <awais.qureshi@arbisoft.com>
2021-07-03 14:22:34 +0500

    Adding django30,31 and 32 in tox. Adding python3.7,3.8,3.9 version for testing. Shiting towards github actions. travis.org is no more running tests.Adding django30,31 and 32 in tox.

--- setup.py
+++ setup.py
@@ -83,6 +83,10 @@ setup(
         "Programming Language :: Python :: 3.3",
         "Programming Language :: Python :: 3.4",
         "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
         "Framework :: Django",
     ],
 )
